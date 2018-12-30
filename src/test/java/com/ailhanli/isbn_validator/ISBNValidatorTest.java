76533985ee8f4c9cbbb8be0960703d0040aae2e1
A I <31765011+superdevelopertr@users.noreply.github.com>
2018-12-30 23:08:32 +0300

    Update ISBNValidatorTest.java

--- src/test/java/com/ailhanli/isbn_validator/ISBNValidatorTest.java
+++ src/test/java/com/ailhanli/isbn_validator/ISBNValidatorTest.java
@@ -10,15 +10,14 @@ package com.ailhanli.isbn_validator;
  * 5- Write test for invalid isbn number. 0140449117
  * 6- Write minumum code to pass test
  * 7- Write test for another valid isbn number. 0140177396
- * 8- Write minumum code to pass test
- * 9- Now refactor code and decide what type to use for valid isbn number
+ * 8- refactor code and write real code for isbn logic and decide what type to use for valid isbn number
  * 
- * 10- write test for not allowed 9 digit numeric isbn number
- * 11- write minumum code to pass test
- * 12- write test for helloworld isbn number is invalid 
- * 13- write minumum code to pass test
- * 14- write test for 012000030X is valid isbn number
- * 15- write minum code to pass test 
+ * 9- write test for not allowed 9 digit numeric isbn number
+ * 10- write minumum code to pass test
+ * 11- write test for helloworld isbn number is invalid 
+ * 12- write minumum code to pass test
+ * 13- write test for 012000030X is valid isbn number
+ * 14- write minum code to pass test 
  *
  */
 
