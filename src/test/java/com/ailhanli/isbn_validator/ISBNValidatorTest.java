e6a993baa3892e0beab64acc15f56fbdcc6e2cb4
Abdullah Abi <abdullah.ilhanli@icloud.com>
2018-12-30 23:06:21 +0300

    init

---
+++ src/test/java/com/ailhanli/isbn_validator/ISBNValidatorTest.java
@@ -0,0 +1,29 @@
+package com.ailhanli.isbn_validator;
+
+/**
+ * 
+ * TODO:for each iteration please follow red-green-refactor cycle...
+ * 1- Write a test for valid isbn number outcome not for business logic. 0140449116
+ * 2- Dont prejudge design use integer isbn for validation
+ * 3- Write minumum code to pass test
+ * 4- Refactor variable name for our code.
+ * 5- Write test for invalid isbn number. 0140449117
+ * 6- Write minumum code to pass test
+ * 7- Write test for another valid isbn number. 0140177396
+ * 8- Write minumum code to pass test
+ * 9- Now refactor code and decide what type to use for valid isbn number
+ * 
+ * 10- write test for not allowed 9 digit numeric isbn number
+ * 11- write minumum code to pass test
+ * 12- write test for helloworld isbn number is invalid 
+ * 13- write minumum code to pass test
+ * 14- write test for 012000030X is valid isbn number
+ * 15- write minum code to pass test 
+ *
+ */
+
+public class ISBNValidatorTest {
+
+	
+	
+}
